84c47227e30639f121eede5dc8fe39c84b243e13
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-03-09 17:22:00 +0300

    Add constructors + print function

--- graph.cpp
+++ graph.cpp
@@ -2,12 +2,86 @@
 // Created by Igor Molchanov on 09.03.2022.
 //
 #include "iostream"
+#include "map"
+#include "vector"
+#include "list"
 
 template<typename T>
-class Graph{
+class Graph {
 public:
     Graph() = default;
-    void check() {
-        std::cout << typeid(T).name() << std::endl;
-    }
+
+    explicit Graph(std::map<T, std::list<T>> &adjacency_map);
+
+    static Graph from_nodes_edges_lists(std::vector<T> &nodes, std::vector<std::list<T>> &edges_lists);
+
+    static Graph from_adjacency_matrix(std::vector<T> &nodes, std::vector<std::vector<int>> &adjacency_matrix);
+
+    void add_node(T node);
+
+    void add_edge(T node1, T node2);
+
+    void print();
+
+private:
+    std::map<T, std::list<T>> adjacency_map = {};
 };
+
+template<typename T>
+Graph<T>::Graph(std::map<T, std::list<T>> &adjacency_map) : adjacency_map(adjacency_map) {
+}
+
+template<typename T>
+void Graph<T>::print() {
+    std::cout << "Graph:\t{" << std::endl;
+    for (auto const&[key, val]: adjacency_map) {
+        std::cout << "\t" << key << ':';
+        for (char child_node: val) {
+            std::cout << (T) child_node << " ";
+        }
+        std::cout << "\t\t\t| " << val.size() << " size" << std::endl;
+    }
+    std::cout << "}" << std::endl;
+}
+
+template<typename T>
+void Graph<T>::add_node(T node) {
+    this->adjacency_map[node] = {};
+}
+
+template<typename T>
+void Graph<T>::add_edge(T node1, T node2) {
+    if (std::find(this->adjacency_map[node1].begin(), this->adjacency_map[node1].end(), node2) ==
+        this->adjacency_map[node1].end()) {
+        this->adjacency_map[node1].push_back(node2);
+    }
+    if (std::find(this->adjacency_map[node2].begin(), this->adjacency_map[node2].end(), node1) ==
+        this->adjacency_map[node2].end()) {
+        this->adjacency_map[node2].push_back(node1);
+    }
+}
+
+template<typename T>
+Graph<T> Graph<T>::from_nodes_edges_lists(std::vector<T> &nodes, std::vector<std::list<T>> &edges_lists) {
+    Graph<T> graph = Graph<T>();
+    for (T node: nodes)
+        graph.add_node(node);
+    for (std::list<T> edge: edges_lists)
+        graph.add_edge(edge.front(), edge.back());
+    return graph;
+}
+
+template<typename T>
+Graph<T> Graph<T>::from_adjacency_matrix(std::vector<T> &nodes, std::vector<std::vector<int>> &adjacency_matrix) {
+    Graph<T> graph = Graph<T>();
+    for (T node: nodes)
+        graph.add_node(node);
+    for (int i = 0; i < adjacency_matrix.size(); ++i) {
+        for (int j = 0; j < adjacency_matrix[i].size(); ++j) {
+            if (adjacency_matrix[i][j] > 0) {
+                graph.add_edge(nodes[i], nodes[j]);
+            }
+        }
+    }
+    return graph;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include "graph.cpp"
 
+int main() {
+//    std::list<char> nodes_list = {'A', 'B', 'C'};
+//    std::list<std::list<int>> adjacency_matrix = {{1, 0, 0}, {0, 0, 0}, {0, 0, 1}};
+//    Graph<char> a = Graph<char>(nodes_list, adjacency_matrix);
 
-int main()
-{
-    Graph<char> d = Graph<char>();
-    d.check();
+    std::map<char, std::list<char>> raw = {{'A', {'B', 'C'}},
+                                           {'B', {'A'}},
+                                           {'C', {'A'}}};
+//    Graph<char> a = Graph<char>(raw);
+
+//    std::vector<char> nodes = {'A', 'B', 'C'};
+//    std::list<std::list<char>> edges_list = {{'A', 'B'},
+//                                             {'B', 'C'}};
+//    Graph<char> a = Graph<char>::from_nodes_edges_lists(nodes_list, edges_list);
+
+//    std::vector<std::vector<int>> matrix = {
+//            {0, 1, 1},
+//            {1, 0, 0},
+//            {1, 0, 0},
+//    };
+    Graph<char> a = Graph<char>(raw);
+    a.print();
     return 0;
 }
