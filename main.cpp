1dfb62d127db1c4acd1e9edfe75b73d0b87bd929
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-03-19 16:45:08 +0300

    Create function to print graph

--- main.cpp
+++ main.cpp
@@ -3,15 +3,27 @@
 
 using namespace std;
 
+    string out;
+    out += "{\n";
+    for (const auto &item: graph.to_dictionary_of_adjacency()) {
+        out += std::to_string(item.first) + ": ";
+        out += "{";
+        for (const auto &_item: item.second)
+            out += std::to_string(_item.first) + ":" + std::to_string(_item.second) + ", ";
+        out += "}, \n";
+    }
+    out += "}";
+    return out;
+}
+
 int main() {
     auto gr = Graph();
-
     gr.add_node(4);
     gr.add_node(5);
+    gr.add_node(6);
     gr.add_edge(4, 5);
+    gr.add_edge(4, 6);
     cout << gr.nodes.size() << endl;
-
-
-    gr.dispose();
+    cout << graph_to_string(gr);
     return 0;
 }
